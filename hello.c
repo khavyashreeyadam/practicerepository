47a8e1aa5f76a3a98fe0132399ad587fa0bfe01c
khavyashreeyadam <36882883+khavyashreeyadam@users.noreply.github.com>
2020-03-09 20:52:10 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
 	// printf() displays the string inside quotation
-	printf("Hello Hi  chitti World  ! \n");
+	printf("Hello Hi  chitti World  muni ! \n");
 	return 0;
 }
